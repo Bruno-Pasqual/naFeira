f2b31720b743ea5710cf7f2d3eb7710eb688b451
b-pasqual <brunopasqual0@gmail.com>
2023-11-29 21:03:01 -0300

    realizando o cadastro dos clientes

---
+++ scripts/salvaUsuario.js
@@ -0,0 +1,13 @@
+const salvaUsuario = () => {
+  const nameInput = document.querySelector('#name-input').value;
+  const passwordInputs = document.querySelectorAll('.password');
+  const addressInput = document.querySelector('address-input');
+  const emailInput = document.querySelector('#email-input');
+  const numberInput = document.querySelector('#number-input');
+
+  console.log(nameInput);
+  console.log(passwordInputs);
+  console.log(addressInput);
+  console.log(emailInput);
+  console.log(numberInput);
+};
