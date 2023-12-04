05a85eaa98a67a2992bf1ce6f85f6abbff4a7683
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-12-04 17:14:39 -0300

    trabalhando na navbar e na navegação entre as páginas

--- scripts/pageCheck.js
+++ scripts/pageCheck.js
@@ -1,4 +1,5 @@
-const pageCheck = (pageName) => {
+const pageCheck = () => {
+  //Fazendo a seleção de todos os links da barra de navegação do site 'nav'
   const links = document.querySelectorAll('.nav-link');
 
   //Atribui a uma variável o caminho para a página atual
@@ -7,12 +8,14 @@ const pageCheck = (pageName) => {
   //Faz um loop nos links selecionados e verifica qual deles bate com o nome da página passado na função (corpo do HTMl) e adiciona ou remove a classe 'active' de acordo com isso
 
   links.forEach((link) => {
+    //acessa o link e faz a atribuição do valor em 'data-value' para uma variável que será utilizada abaixo.
+
     const paginaDoLink = link.getAttribute('data-value');
 
-    if (pageName != paginaDoLink) {
-      link.classList.remove('active');
-    } else {
+    if (currentPage.includes(paginaDoLink)) {
       link.classList.add('active');
+    } else {
+      link.classList.remove('active');
     }
   });
 };
