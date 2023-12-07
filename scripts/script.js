daddea565f16ac21c832c272621885cffecaf34a
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-12-07 08:28:29 -0300

    modais ok

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,41 @@
+//Script do primeiro modal
+
 const createAccountBtn = document.querySelector('#create-btn');
 const createAccountModal = document.querySelector('#create-account-modal');
-const botaoVoltarCreateAccount = document.querySelector('#voltar-btn-create');
+const botoesRetorno = document.querySelectorAll('.return-btn');
 const cadastrarBtn = document.querySelector('#cadastrar-btn');
 
 createAccountBtn.addEventListener('click', () => {
   createAccountModal.showModal();
 });
 
-botaoVoltarCreateAccount.addEventListener('click', () => {
-  createAccountModal.close();
+botoesRetorno.forEach((botao) => {
+  botao.addEventListener('click', () => {
+    createAccountModal.close();
+    loginModal.close();
+  });
 });
 
 cadastrarBtn.addEventListener('click', () => {
   createAccountModal.close();
+});
+
+// Script do segundo modal
+
+const loginModal = document.querySelector('#login-modal');
+const loginBtn = document.querySelector('#entrar-btn');
+const logarBtn = document.querySelector('#logar-btn');
+const criarContaLoginBtn = document.querySelector('#login-criar-conta-btn');
+
+loginBtn.addEventListener('click', () => {
+  loginModal.showModal();
+});
+
+criarContaLoginBtn.addEventListener('click', () => {
+  loginModal.close();
+  createAccountModal.showModal();
+});
+
+logarBtn.addEventListener('click', () => {
+  loginModal.close();
 });
