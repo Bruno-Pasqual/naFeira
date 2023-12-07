08a3a806733efd366828686b79cd213bfafbb3b9
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-12-07 17:30:01 -0300

    teste final

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,40 @@
-//Script do primeiro modal
-
+//modal 1
 const createAccountBtn = document.querySelector('#create-btn');
 const createAccountModal = document.querySelector('#create-account-modal');
 const botoesRetorno = document.querySelectorAll('.return-btn');
 const cadastrarBtn = document.querySelector('#cadastrar-btn');
+
+// modal 2
+
+const loginModal = document.querySelector('#login-modal');
+const loginBtn = document.querySelector('#entrar-btn');
+const logarBtn = document.querySelector('#logar-btn');
+const criarContaLoginBtn = document.querySelector('#login-criar-conta-btn');
+
+const mobileModal = document.querySelector('#mobile-buttons-modal');
+const userButton = document.querySelector('#user-icon');
+const loginMobileBtn = document.querySelector('#mobile-login-btn');
+const createAccountMobileBtn = document.querySelector(
+  '#createAccount-mobile-btn'
+);
+
+// Script modal mobile
+
+userButton.addEventListener('click', () => {
+  mobileModal.showModal();
+});
+
+createAccountMobileBtn.addEventListener('click', () => {
+  mobileModal.close();
+  createAccountModal.showModal();
+});
+
+loginMobileBtn.addEventListener('click', () => {
+  mobileModal.close();
+  loginModal.showModal();
+});
+
+//script primeiro modal
 
 createAccountBtn.addEventListener('click', () => {
   createAccountModal.showModal();
@@ -21,11 +52,6 @@ cadastrarBtn.addEventListener('click', () => {
 });
 
 // Script do segundo modal
-
-const loginModal = document.querySelector('#login-modal');
-const loginBtn = document.querySelector('#entrar-btn');
-const logarBtn = document.querySelector('#logar-btn');
-const criarContaLoginBtn = document.querySelector('#login-criar-conta-btn');
 
 loginBtn.addEventListener('click', () => {
   loginModal.showModal();
